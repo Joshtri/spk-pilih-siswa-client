7abf2f685c94f572a795a876c864538d52001ce9
Joshtri <stuffofyos1516@gmail.com>
2024-12-21 15:25:52 +0800

    updating route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import DashboardPage from './pages/DashboardPage'
 import KriteriaPage from './pages/Kriteria/KriteriaPage'
+import KelasPage from './pages/Kelas/KelasPage'
+import SiswaPage from './pages/Siswa/SiswaPage'
 // import DataKriteriaPage from './pages/kriteria/DataKriteriaPage'
 // import DataSiswaPage from './pages/siswa/DataSiswaPage'
 
@@ -17,6 +19,8 @@ function App() {
         <Routes>
           <Route path='/dashboard' element={<DashboardPage/>} />
           <Route path='/kriteria' element={<KriteriaPage/>}/>
+          <Route path='/kelas' element={<KelasPage/>}/>
+          <Route path='/siswa' element={<SiswaPage/>}/>
           {/* <Route path='/kriteria' element={<DataKriteriaPage/>}/>
           <Route path='/siswa' element={<DataSiswaPage/>}/> */}
         </Routes>
