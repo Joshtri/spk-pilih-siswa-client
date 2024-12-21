03e752f3b408bbdfa1a819d5a854b9439e39a6ce
Joshtri <stuffofyos1516@gmail.com>
2024-12-21 20:32:19 +0800

    updaitng dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from './Layout';
 import RankingTable from '../components/Dashboard/RankingTable';
 import CriteriaSummary from '../components/Dashboard/CriteriaSummary';
@@ -6,8 +6,14 @@ import EvaluationHistory from '../components/Dashboard/EvaluationHistory';
 import StatisticsCard from '../components/Dashboard/StatisticsCard';
 
 const DashboardPage: React.FC = () => {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
-    <Layout>
+    <Layout isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}>
       <h1 className="text-center text-black text-2xl font-bold">Dashboard Pemilihan Siswa Berprestasi</h1>
       <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {/* Statistik Siswa */}
